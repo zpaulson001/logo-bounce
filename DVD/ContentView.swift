97f97fbe47215de066eb2b36d8d2e902dcbbe5d2
Zac <zpaulson001@gmail.com>
2026-03-05 09:59:27 -0800

    Initial Commit

---
+++ DVD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DVD
+//
+//  Created by Zac Paulson on 3/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DVD/DVDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DVDApp.swift
+//  DVD
+//
+//  Created by Zac Paulson on 3/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DVDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
