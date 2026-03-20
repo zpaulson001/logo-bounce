b55a8b649be10da566640a084fb57a9d6c0a8d20
Zac <zpaulson001@gmail.com>
2026-03-20 13:19:41 -0700

    use LogoPhysics store for animation logic

--- LogoBounce/BouncingLogoCanvas.swift
+++ LogoBounce/BouncingLogoCanvas.swift
@@ -10,50 +10,44 @@ import SwiftUI
 struct BouncingLogoCanvas: View {
 
     @State private var isResizing = false
-
-    let speed: CGFloat = 300
-
-    // The "Ping-Pong" function: Maps linear time to a bouncing coordinate
-    private func calculateBounce(time: Double, speed: CGFloat, max: CGFloat)
-        -> CGFloat
-    {
-        let totalDistance = time * Double(speed)
-        let phase = totalDistance.truncatingRemainder(
-            dividingBy: Double(max * 2)
-        )
-        return CGFloat(phase > Double(max) ? Double(max * 2) - phase : phase)
-    }
+    @State private var logoPhysicsStore: LogoPhysicsStore = .init()
+    @Environment(MainToolbarSettings.self) private var toolbarSettings
 
     var body: some View {
-        TimelineView(.animation(paused: isResizing)) { (timeline: TimelineViewDefaultContext) in
+        TimelineView(.animation(paused: isResizing)) {
+            (timeline: TimelineViewDefaultContext) in
 
             Canvas { gc, size in
-                let time = timeline.date.timeIntervalSinceReferenceDate
-                let logo = gc.resolve(
+                var logo = gc.resolve(
                     Image(
-                        "jedediah_logo"
+                        toolbarSettings.selectedLogo
                     )
                 )
 
-                let logoSize = CGSize(
-                    width: logo.size.width / 4,
-                    height: logo.size.height / 4
-                )
+                let desiredLogoWidth =
+                    logo.size.width
+                    / (logo.size.height / toolbarSettings.desiredLogoHeight)
 
-                let x = calculateBounce(
-                    time: time,
-                    speed: speed,
-                    max: size.width - logoSize.width
+                logoPhysicsStore.logoSize.width = desiredLogoWidth
+                logoPhysicsStore.logoSize.height =
+                    toolbarSettings.desiredLogoHeight
+
+                logoPhysicsStore.update(
+                    to: timeline.date,
+                    within: size,
+                    baseSpeed: toolbarSettings.animationSpeed
                 )
-                let y = calculateBounce(
-                    time: time,
-                    speed: speed,
-                    max: size.height - logoSize.height
-                )
+                logo.shading = .color(logoPhysicsStore.logoColor)
 
                 let rect = CGRect(
-                    origin: CGPoint(x: x, y: y),
-                    size: logoSize
+                    origin: CGPoint(
+                        x: logoPhysicsStore.position.x,
+                        y: logoPhysicsStore.position.y
+                    ),
+                    size: CGSize(
+                        width: desiredLogoWidth,
+                        height: toolbarSettings.desiredLogoHeight
+                    )
                 )
 
                 gc.draw(
